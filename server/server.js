7d864d9f38c854b351c8634552081da80d6d4705
charles <charlesgintoki@gmail.com>
2025-08-26 22:47:34 +0800

    done on userController getUsersData

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import cookieParser from "cookie-parser"
 import connectDb from "./config/mongodb.js"
 import authRouter from "./routes/authRoutes.js"
+import userRouter from "./routes/userRoutes.js"
 
 const app = express();
 const port = process.env.PORT  || 4000;
@@ -22,6 +23,9 @@ app.get('/', (req,res) => {
 
 // API ROUTES / ENDPOINT
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+
+
 
 
 
