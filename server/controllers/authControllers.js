323649b23c4bb67123bf6ea41406b50a5e8fbf75
charles <charlesgintoki@gmail.com>
2025-08-26 16:21:08 +0800

    done on sending welcome email once user registered

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModal.js';
+import transporter from '../config/nodemailer.js';
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -31,6 +32,17 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       maxAge: 1 * 24 * 60 * 60 * 1000
     })
+
+    
+    //Sending Welcome Email using nodemailer
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: 'Welcome! to Charles Dev',
+      text: `Welcome to Charles Dev website, Your account has been created with email id: ${email}`
+    }
+
+    await transporter.sendMail(mailOptions);
 
     return res.json({ success: true })
 
@@ -65,6 +77,8 @@ export const login = async (req, res) => {
       maxAge: 1 * 24 * 60 * 60 * 1000
     })
 
+
+
     return res.json({ success: true })
 
   } catch (error) {
@@ -75,13 +89,13 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie('token',{
+    res.clearCookie('token', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     })
 
-    return res.json({success:true, message:"Logged Out"})
+    return res.json({ success: true, message: "Logged Out" })
 
   } catch (error) {
     return res.json({ success: true, message: error.message })
