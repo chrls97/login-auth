7c01551871c0d8ad089f3cd2b958344adc267eb8
charles <charlesgintoki@gmail.com>
2025-09-01 23:03:44 +0800

    update the cors and allowed origins

--- server/server.js
+++ server/server.js
@@ -10,11 +10,23 @@ const app = express();
 const port = process.env.PORT  || 4000;
 connectDb()
 
-const allowedOrigins = process.env.FRONTEND_URL || ['http://localhost:5173']
+const allowedOrigins = [
+  process.env.FRONTEND_URL,  // e.g. "https://your-frontend.vercel.app"
+  "http://localhost:5173"
+].filter(Boolean); // removes undefined
 
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 
 app.get('/', (req,res) => {
